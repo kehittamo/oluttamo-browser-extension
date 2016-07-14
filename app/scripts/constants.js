674002058a9af87e99744d34e6d8a77802f4f2ad
Janne Saarela <janne.juhani@gmail.com>
2016-07-14 14:44:17 +0300

    Add new constants

--- app/scripts/constants.js
+++ app/scripts/constants.js
@@ -1,4 +1,7 @@
-export const SEARCH_KEY_NAME = "selected_text";
-export const REACT_ROOT_DIV_ID = "oluttamo-content";
 export const BEER_API_URL = "https://oluttamo.herokuapp.com/api";
 export const BEER_API_SEARCH_PREFIX = "/search/";
+export const OLUTTAMO_SEARCH_SHORTCUT ="search-beer";
+export const RATE_BEER_URL = "https://ratebeer.com";
+export const RATE_BEER_GET_BEER_PREFIX = "/beer/";
+export const REACT_ROOT_DIV_ID = "oluttamo-content";
+export const SEARCH_KEY_NAME = "selected_text";
