22be09a984cb2b403b41c220d5b764d56c827638
Janne Saarela <janne.juhani@gmail.com>
2016-07-14 14:45:27 +0300

    Add shortcut and changes[SEARCH_KEY_NAME] checks

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,6 +1,6 @@
 "use strict";
 
-import { SEARCH_KEY_NAME } from "./constants";
+import { SEARCH_KEY_NAME, OLUTTAMO_SEARCH_SHORTCUT } from "./constants";
 
 // Enable chromereload by uncommenting this line:
 import "./lib/livereload";
@@ -11,23 +11,22 @@ chrome.runtime.onInstalled.addListener(function (details) {
     createMenu();
 });
 
-// TODO: executeScript only if shortcut is "search-beer"
 chrome.commands.onCommand.addListener(shortcut => {
-    console.log("shortcut: ", shortcut);
-    chrome.tabs.executeScript(null, {
-        file: "./scripts/getSelection.js",
-        runAt: "document_end",
-    });
+    if (shortcut === OLUTTAMO_SEARCH_SHORTCUT){
+        chrome.browserAction.setBadgeText({text: "<3"});
+        chrome.tabs.executeScript(null, {
+            file: "./scripts/getSelection.js",
+            runAt: "document_end",
+        });
+    }
 });
 
-// TODO: executeScript only if changes include SEARCH_KEY_NAME
 chrome.storage.onChanged.addListener(function(changes, namespace) {
-    console.log("changes: ", changes);
-    chrome.storage.local.get(SEARCH_KEY_NAME, (items) => {
-        console.log(`Value: ${items[SEARCH_KEY_NAME]}`);
+    if (changes[SEARCH_KEY_NAME]){
+        chrome.browserAction.setBadgeText({text: "<3"});
         chrome.tabs.executeScript(null, {
             file: "./scripts/search.js",
             runAt: "document_end",
         });
-    });
+    }
 });
