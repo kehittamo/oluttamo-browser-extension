6664e942071609b7fa8e70dd9ae717120263ea28
Janne Saarela <janne.juhani@gmail.com>
2016-07-14 14:42:37 +0300

    Run search() also if selection text is same

--- app/scripts/lib/setSelection.js
+++ app/scripts/lib/setSelection.js
@@ -1,7 +1,15 @@
 import { SEARCH_KEY_NAME } from "../constants";
+import getCurrentSearchString from "./getCurrentSearchString";
+import search from "../search";
 
 function setSelection(selectionText) {
-    chrome.storage.local.set({[SEARCH_KEY_NAME]: selectionText}, function() {});
+    getCurrentSearchString((searchQuery) =>{
+        if (searchQuery === selectionText){
+            search();
+        } else {
+            chrome.storage.local.set({[SEARCH_KEY_NAME]: selectionText}, function() {});
+        }
+    });
 }
 
 export default setSelection;
