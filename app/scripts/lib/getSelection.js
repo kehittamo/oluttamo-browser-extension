d03fdd77270732f7ecb66fb4c8b42100d86e96d7
Janne Saarela <janne.juhani@gmail.com>
2016-07-14 14:39:59 +0300

    Remove unused files

--- app/scripts/lib/getSelection.js
+++
@@ -1,9 +0,0 @@
-import setSelection from "./setSelection";
-
-const selection = window.getSelection().toString();
-
-console.log("selection: ", selection);
-
-if(selection.length > 0){
-    setSelection(selection);
-}
