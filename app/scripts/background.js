4198fde2aff970b437de7dbfebb52e7d7c1f4afd
Janne Saarela <janne.juhani@gmail.com>
2016-07-15 11:16:02 +0300

    Add setBadgeDetails func

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,19 +1,19 @@
 "use strict";
 
-import { SEARCH_KEY_NAME, OLUTTAMO_SEARCH_SHORTCUT } from "./constants";
+import { BEER_KEY_NAME, SEARCH_KEY_NAME, OLUTTAMO_SEARCH_SHORTCUT } from "./constants";
 
 // Enable chromereload by uncommenting this line:
-// import "./lib/livereload";
+import "./lib/livereload";
 
 import createMenu from "./lib/contextMenus";
 
-chrome.runtime.onInstalled.addListener(function (details) {
+chrome.runtime.onInstalled.addListener((details) => {
     createMenu();
 });
 
 chrome.commands.onCommand.addListener(shortcut => {
     if (shortcut === OLUTTAMO_SEARCH_SHORTCUT){
-        chrome.browserAction.setBadgeText({text: "<3"});
+        setBadgeDetails("...", "#8CF0C8");
         chrome.tabs.executeScript(null, {
             file: "./scripts/getSelection.js",
             runAt: "document_end",
@@ -21,12 +21,19 @@ chrome.commands.onCommand.addListener(shortcut => {
     }
 });
 
-chrome.storage.onChanged.addListener(function(changes, namespace) {
+chrome.storage.onChanged.addListener((changes, namespace) => {
     if (changes[SEARCH_KEY_NAME]){
-        chrome.browserAction.setBadgeText({text: "<3"});
+        setBadgeDetails("...", "#8CF0C8");
         chrome.tabs.executeScript(null, {
             file: "./scripts/search.js",
             runAt: "document_end",
         });
+    } else if (changes[BEER_KEY_NAME]){
+        setBadgeDetails("<3", "#9669AF");
     }
 });
+
+function setBadgeDetails(text, color){
+    chrome.browserAction.setBadgeText({text: text});
+    chrome.browserAction.setBadgeBackgroundColor({color: color});
+}
