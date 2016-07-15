def576056d3929bac0e9e941784fcf1a88f4af88
Janne Saarela <janne.juhani@gmail.com>
2016-07-15 11:15:09 +0300

    Move render to own function, add also beer data to storage

--- app/scripts/search.js
+++ app/scripts/search.js
@@ -1,26 +1,39 @@
 import React from "react";
 import { render } from "react-dom";
 import SearchResults from "./components/SearchResults";
-import { SEARCH_KEY_NAME, REACT_ROOT_DIV_ID, BEER_API_URL, BEER_API_SEARCH_PREFIX } from "./constants";
+import { BEER_KEY_NAME, SEARCH_KEY_NAME, REACT_ROOT_DIV_ID, BEER_API_URL, BEER_API_SEARCH_PREFIX } from "./constants";
 import getCurrentSearchString from "./lib/getCurrentSearchString";
+import setData from "./lib/setData";
 
+/*
+* Search beer data from Oluttamo API
+*/
 function search(){
     getCurrentSearchString((searchQuery) =>{
-        createRootDiv(()=>{
+        if(searchQuery){
             const httpRequest = new XMLHttpRequest();
             httpRequest.onreadystatechange = (data) => {
                 // TODO: handle errors :)
                 if(httpRequest.readyState === XMLHttpRequest.DONE && httpRequest.status === 200) {
                     const json = JSON.parse(httpRequest.responseText);
-                    render(<SearchResults q={searchQuery} data={json}/>, document.getElementById(REACT_ROOT_DIV_ID));
+                    setData(BEER_KEY_NAME, {query: searchQuery, data: json, url: window.location.href});
+                    addSearchResults(searchQuery, json);
                 };
             };
             httpRequest.open("GET", `${BEER_API_URL}${BEER_API_SEARCH_PREFIX}${searchQuery}`);
             httpRequest.send();
-        });
+        }
     });
 }
 
+// render search results react element
+function addSearchResults(searchQuery, data, showOnlyFooter=false){
+    createRootDiv(()=>{
+        render(<SearchResults q={searchQuery} data={data} showOnlyFooter={showOnlyFooter}/>, document.getElementById(REACT_ROOT_DIV_ID));
+    });
+}
+
+// Create react root div it doesn't exist yet
 const createRootDiv = (cb) => {
     const elementExists = document.getElementById(REACT_ROOT_DIV_ID);
     if(!elementExists){
