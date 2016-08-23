4f16b7513354043a3f2d34d0ea3fff42128f4aa7
Janne Saarela <janne.juhani@gmail.com>
2016-08-23 17:37:00 +0300

    Remove unused

--- app/scripts/components/TextSearch.jsx
+++ app/scripts/components/TextSearch.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import DebounceInput from "react-debounce-input";
 import { SEARCH_KEY_NAME, BEER_KEY_NAME, RATE_BEER_GET_BEER_PREFIX, RATE_BEER_URL } from "../constants";
-import ajaxSearch from "../lib/ajaxSearch";
 import setData from "../lib/setData";
 
 const TextSearch = React.createClass({
